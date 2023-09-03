918b4e409bc014c8d2691a687b41ed8b45ae9cb0
Lalo Martins <lalo.martins@gmail.com>
2023-09-03 20:57:16 +0900

    script to create an empty .g.dart file

--- bin/initialize.dart
+++ bin/initialize.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+const template = """
 // Dummy CommitInfo file which you can commit with your source code, so that your editor
 // won't complain about missing files or declarations
 class CommitInfo {
@@ -20,3 +23,9 @@ class CommitInfo {
 
 // ignore: unnecessary_nullable_for_final_variable_declarations
 const CommitInfo? commitInfo = null;
+""";
+
+void main() {
+  print("Initializing package");
+  File("lib/commit_info.g.dart").writeAsStringSync(template);
+}
